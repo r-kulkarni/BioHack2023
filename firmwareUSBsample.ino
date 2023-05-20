bfdac868164bcd307d881b5e4c47314521f16e78
Rahul Kulkarni <42728025+r-kulkarni@users.noreply.github.com>
2023-05-20 16:10:05 -0700

    usb connection

---
+++ firmwareUSBsample.ino
@@ -0,0 +1,5 @@
+#define BLYNK_TEMPLATE_ID "TMPL2rQv0mCLk"
+#define BLYNK_TEMPLATE_NAME "Quickstart Template"
+#define BLYNK_AUTH_TOKEN "rMs6j9OhRPhl1_FV5z-8RAhh_-wypM4E"
+char ssid [] = "Navier";
+char pass [] = "Rahul135";
